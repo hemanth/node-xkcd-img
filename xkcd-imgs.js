c880585858545ec5f74a41b574aa0d885f0b7c6c
hemanth.hm <hemanth.hm@gmail.com>
2014-04-06 01:47:32 +0530

    Major clean up

--- xkcd-imgs.js
+++ xkcd-imgs.js
@@ -1,26 +1,25 @@
 var request = require('request');
-var rand = '';
-request('http://xkcd.com/info.0.json',function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-         var max_num = JSON.parse(body)["num"];
-         rand =  Math.floor((Math.random()*max_num)+1);
-         if(rand == 404) { rand = get_num();} // Avoid 404 page ;)
-        }
-});
-
 var img = function(cb){
-    var xkcd_url = 'http://xkcd.com/'+rand+'/info.0.json';
-    request(xkcd_url, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            comic = JSON.parse(body);
-            img_url = comic['img'];
-            img_title = comic['alt'];
-            cb({
-              'url': img_url,
-              'title': img_title
+      request('http://xkcd.com/info.0.json',function (error, response, body) {
+           if (!error && response.statusCode == 200) {
+               var max_num = JSON.parse(body)["num"];
+               rand =  Math.floor((Math.random()*max_num)+1);
+               console.log(rand)
+               if(rand == 404) { rand = get_num();} // Avoid 404 page ;)
+            }
+            var xkcd_url = 'http://xkcd.com/'+rand+'/info.0.json';
+            request(xkcd_url, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    comic = JSON.parse(body);
+                    img_url = comic['img'];
+                    img_title = comic['alt'];
+                    cb({
+                      'url': img_url,
+                      'title': img_title
+                    });
+                }
             });
-        }
-    });
+      });
 }
 
 exports.img = img;
