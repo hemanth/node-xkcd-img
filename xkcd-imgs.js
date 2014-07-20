efb65396aa7a1b48b8d6e33b37e61572ecd2dc1e
Ali Karbassi <ali@karbassi.com>
2014-07-20 17:44:55 -0500

    Remove console.log

--- xkcd-imgs.js
+++ xkcd-imgs.js
@@ -4,8 +4,7 @@ var img = function(cb){
            if (!error && response.statusCode == 200) {
                var max_num = JSON.parse(body)["num"];
                rand =  Math.floor((Math.random()*max_num)+1);
-               console.log(rand)
-               if(rand == 404) { rand = get_num();} // Avoid 404 page ;)
+               if(rand == 404) { rand = get_num(); } // Avoid 404 page ;)
             }
             var xkcd_url = 'http://xkcd.com/'+rand+'/info.0.json';
             request(xkcd_url, function (error, response, body) {
