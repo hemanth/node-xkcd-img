ba6d1df94494d5e00c6e02738abf0168e8737a7c
Hemanth.HM <hemanth.hm@gmail.com>
2016-03-20 11:14:48 +0530

    lint and format fixes

--- xkcd-imgs.js
+++ xkcd-imgs.js
@@ -1,28 +1,28 @@
 var request = require('request');
-var img = function(cb){
-      request('http://xkcd.com/info.0.json',function (error, response, body) {
-           if (!error && response.statusCode == 200) {
-               var max_num = JSON.parse(body)["num"];
-               rand =  Math.floor((Math.random()*max_num)+1);
-               if(rand == 404) { rand = get_num(); } // Avoid 404 page ;)
-            }
-            var xkcd_url = 'http://xkcd.com/'+rand+'/info.0.json';
-            request(xkcd_url, function (error, response, body) {
+var img = function (cb) {
+    request('http://xkcd.com/info.0.json', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var max_num = JSON.parse(body)['num'];
+            var rand =  Math.floor((Math.random() * max_num) + 1);
+            if (rand == 404) { rand = get_num(); } // Avoid 404 page ;)
+        }
+
+        var xkcd_url = 'http://xkcd.com/' + rand + '/info.0.json';
+        request(xkcd_url, function (error, response, body) {
                 if (!error && response.statusCode == 200) {
                     comic = JSON.parse(body);
                     img_url = comic['img'];
                     img_title = comic['alt'];
-                    return cb(null,{
-                      'url': img_url,
-                      'title': img_title
+                    return cb(null, {
+                        url: img_url,
+                        title: img_title
                     });
-                } 
+                }
+
                 cb(new Error(error), null);
             });
-      });
-}
+    });
+};
 
 exports.img = img;
-
-
 
